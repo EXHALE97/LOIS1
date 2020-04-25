01187d189ac54fbabd2c95a4390c3469ce5e4c71
Yulya Evgrafova <yulyaevrafova92@gmail.com>
2020-04-25 12:32:10 +0300

    Added lab

---
+++ scripts/scriptOperation.js
@@ -0,0 +1,33 @@
+function clickCheckFormula() {
+    var inputFormula = document.getElementById("inputFormula").value;
+    var output = document.getElementById("output");
+    var outputTypeFormula = document.getElementById("outputTypeFormula");
+      if (verificationFormula(inputFormula)) {
+        document.getElementById("hiddenForm").hidden = false;
+          output.innerHTML = "Данное выражение является формулой";
+      } else {
+         document.getElementById("hiddenForm").hidden = true;
+          output.innerHTML = "Данное выражение не является формулой";
+      }
+}
+// Функции для тестов
+  function clickYes() {
+  var outputResult = document.getElementById("outputResult");
+  var enterFormula = document.getElementById("EnterFormula").value;
+  if (verificationFormula(enterFormula)) {
+  outputResult.innerHTML = "Верно";
+      } else {
+      outputResult.innerHTML = "Неверно";
+      }
+
+  }
+
+  function clickNo() {
+    var outputResult = document.getElementById("outputResult");
+    var enterFormula = document.getElementById("EnterFormula").value;
+    if (verificationFormula(enterFormula)) {
+    outputResult.innerHTML = "Неверно";
+        } else {
+        outputResult.innerHTML = "Верно";
+        }
+  }
